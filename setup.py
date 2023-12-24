813907550461e07fc118d7b116ac8972dbed1c83
ranjith <ranjithkumarneeruganti@gmail.com>
2023-12-25 00:51:09 +0530

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='It is made in the process of learning MLOPS',
+    author='ranjith kumar N',
+    license='MIT',
+)
